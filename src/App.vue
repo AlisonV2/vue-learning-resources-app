c0636d3cf8127fb92827876f7cdb6b65b502c393
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-01 09:59:29 +0200

    created the app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <app-header title="Forget me not"></app-header>
+  <all-resources></all-resources>
+</template>
+
+<script>
+import AppHeader from './components/layouts/AppHeader.vue';
+import AllResources from './components/learning-resources/AllResources.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AllResources
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode']
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #E55689;
+  border: 1px solid #E55689;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #5E4FA5;
+  border-color: #5E4FA5;
+}
+
+.flat {
+  background-color: transparent;
+  color: #5E4FA5;
+  border: 1px solid #5E4FA5;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #5E4FA5;
+  color: white;
+}
+</style>
